713c4990819e3c6c29d1e74d6b3a8f20a97e2f1f
nobody <nobody@745caa7e-1953-11de-bcda-2bb9902f8029>
2008-11-05 18:32:18 +0000

    Gets the transform (simple text table), via STAMP, for transforming one PDB chain onto another.

---
+++ perl/EMBL/transform.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+SRC=`echo $1 | tr "[:upper:]" "[:lower:]" `
+REF=`echo $2 | tr "[:upper:]" "[:lower:]" `
+#DST=`echo $3 | tr "[:upper:]" "[:lower:]" `
+
+DIR=`mktemp -t -d assembly.tmp.XXXX`
+
+cd $DIR
+#echo $PWD
+
+pdbc -d $SRC > $SRC.dom
+pdbc -d $REF > $REF.dom
+#pdbc -d $DST > $DST.dom
+
+cat $SRC.dom $REF.dom > $SRC-$REF.dom
+do_stamp $SRC-$REF.dom > $SRC-$REF.trans
+# NB $REF must have a lowercase chain ID
+pickframe -f $SRC-$REF.trans -i $REF > $SRC-$REF-FoR.trans
+
+# This is the transformation
+# But it still has a } character at the end
+
+# Whole domain block
+#cat $SRC-$REF-FoR.trans | egrep -v "(%|0.00000|$REF)"
+
+# Or just the transformation matrix
+cat $SRC-$REF-FoR.trans | egrep -v "(%|0.00000|$SRC|$REF)" | tr -d '}'
+
