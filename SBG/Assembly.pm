de813a65654e79b905d5d13204099405710c9e52
nobody <nobody@745caa7e-1953-11de-bcda-2bb9902f8029>
2009-01-03 18:37:27 +0000

    Imported code from assemble.pl but hasn't been sorted out. Doesn't compile.

--- SBG/Assembly.pm
+++ SBG/Assembly.pm
@@ -33,8 +33,8 @@ field 'clash';
 
 
 use overload (
-    '""' => 'asstring',
-    'eq' => 'eq',
+    '""' => '_asstring',
+    'eq' => '_eq',
     );
 
 
@@ -56,23 +56,8 @@ sub new () {
 
     $self->{clash} = $config->val('assembly', 'clash') || '30';
 
-    # Component L<SBG::Domain>s in this Assembly
-    my $comp = {};
-    # L<SBG::Interaction>s used in this Assembly
-    my $iaction = {};
-
     return $self;
 } # new
-
-
-sub asstring {
-    join ",", sort keys %{$self->{iaction}};
-}
-
-sub eq {
-    my $other = shift;
-    return "$self" eq "$other";
-}
 
 
 ################################################################################
@@ -97,11 +82,22 @@ incurring a cloning copy penalty.
 
 =cut
 sub clone {
-    super(shift || 2);
+    $self->Clone::clone(shift || 2);
 } # clone
 
 
-# Number of components in this assembly
+################################################################################
+=head2 size
+
+ Title   : size
+ Usage   : $assembly->size;
+ Function: Number of components in the current Assembly
+ Example : $assembly->size;
+ Returns : ents in the current Assembly
+ Args    : NA
+
+
+=cut
 sub size {
     return scalar(keys %{$self->{comp}});
 }
@@ -128,15 +124,19 @@ NB Spiffy doesn't magically create $self here, probably due to the attribute
 =cut
 sub comp : lvalue {
     my ($self,$key) = @_;
+    $self->{comp} ||= {};
     # Do not use 'return' with 'lvalue'
     $self->{comp}{$key};
 } # comp
 
+
 sub iaction : lvalue {
     my ($self,$key) = @_;
+    $self->{iaction} ||= {};
     # Do not use 'return' with 'lvalue'
     $self->{iaction}{$key};
 }
+
 
 ################################################################################
 =head2 clashes
@@ -174,5 +174,166 @@ sub clashes {
 } # clashes
 
 
+sub _asstring {
+    join ",", sort keys %{$self->{iaction}};
+}
+
+
+sub _eq {
+    my $other = shift;
+    return "$self" eq "$other";
+}
+
+
+
+
+
+
+
+
+sub lastedge {
+    my ($self, $traversal) = @_;
+    my $io = new AssemblIO();
+    $io->write($self) if $self->size() > 2;
+}
+
+
+sub lastnode {
+    my ($self, $traversal) = @_;
+    my $io = new AssemblIO();
+    $io->write($self) if $self->size() > 2;
+}
+
+
+
+
+# Callback for attempting to add a new interaction template
+# Gets an assembly object as a status object
+sub try_edge {
+#     my ($u, $v, $traversal, $ix_index) = @_;
+    my ($u, $v, $traversal, $assembly) = @_;
+
+#     $ix_index ||= 0;
+
+    print STDERR "\ttry_edge $u $v:\n";
+    my $g = $traversal->{graph};
+
+    # IDs of Interaction's (templates) in this Edge
+    my @ix_ids = $g->get_edge_attribute_names($u, $v);
+    @ix_ids = sort @ix_ids;
+
+    # Extract current state of this edge, if any
+    my $edge_id = "$u--$v";
+    # Which of the interaction templates, for this edge, to try (next)
+    my $ix_index = $traversal->get_state($edge_id . "ix_index") || 0;
+
+    # If no templates (left) to try, cannot use this edge
+    unless ($ix_index < @ix_ids) {
+        print STDERR "\tNo more templates\n";
+        # Now reset, for any subsequent, independent attempts on this edge
+        $traversal->set_state($edge_id . "ix_index", 0);
+        return undef;
+    }
+
+    # Try next interaction template
+    my $ix_id = $ix_ids[$ix_index];
+    print STDERR "\ttemplate ", 1+$ix_index, "/" . @ix_ids . "\n";
+    my $ix = $g->get_interaction_by_id($ix_id);
+#     print STDERR "$ix ";
+
+    # Structural compatibility test (backtrack on failure)
+    my $success = try_interaction3($assembly, $ix, $u, $v);
+
+
+#     $traversal->set_state($edge_id . "success", $success);
+
+    # Next interaction iface to try on this edge
+    $ix_index++;
+    $traversal->set_state($edge_id . "ix_index", $ix_index);
+
+    print STDERR "\n";
+
+    if ($success) {
+
+        # Add this template to progressive solution
+        $assembly->add($ix_id);
+        return 1;
+
+    } else {
+        # This means failure, 
+        # This is not the same as exhausting the templates (that's undef)
+        return -1;
+        # I.e. do not recurse here
+    }
+
+} # try_edge
+
+
+
+
+
+# TODO DOC:
+# Uses the hash saved in the interation object (set when templates loaded) to find out what templates used by which components on and edge in the interaction graph
+sub try_interaction3 {
+    my ($assembly, $iaction, $src, $dest) = @_;
+    my $success = 0;
+
+    # Lookup $src in $iaction to identify its monomeric template domain
+    my $srcdom = $iaction->{template}{$src};
+    my $destdom = $iaction->{template}{$dest};
+    print STDERR "\t$src($srcdom)->$dest($destdom)\n";
+
+    # Get reference domain of $src 
+    my $srccofm = $assembly->cofm($src);
+
+    unless (defined $srccofm) {
+        # base case: no previous structural constraint, implicitly sterically OK
+        $srccofm = new SBG::CofM($src, $srcdom);
+        # Save CofM object for src component in assembly, indexed by $src
+        $assembly->cofm($src, $srccofm);
+        my $destcofm =  new SBG::CofM($dest, $destdom);
+        $assembly->cofm($dest, $destcofm);
+        return $success = 1;
+    }
+
+    # Find the frame of reference for the source
+    # STAMP dom identifier (PDBID/CHAINID), TODO should be a descriptor
+    my $refdom = $srccofm->id;
+
+    # Superpose this template dom of the src component onto the reference dom
+    # TODO abstract this into a DB cache as well
+    my $nexttrans = stampfile($srcdom, $refdom);
+    if (! defined $nexttrans) { 
+        return $success = 0; 
+    }
+
+    # Then apply that transformation to the interaction partner $dest
+    # Get CofM of dest template domain (the one to be transformed)
+    # NB Any previous $assembly->cofm($dest) gets overwritten
+    my $destcofm =  new SBG::CofM($dest, $destdom);
+
+    # Product of relative with absolute transformation
+    # TODO DOC order of mat. mult.
+    $destcofm->apply($srccofm->cumulative * $nexttrans);
+
+    # Check new coords of dest for clashes across currently assembly
+    $success = ! $assembly->clashes($destcofm);
+    if ($success) {
+        # Update frame-of-reference of interaction partner
+        $assembly->cofm($dest, $destcofm);
+    }
+
+    print STDERR "\ttry_interaction ", $success ? "succeeded" : "failed", "\n";
+    return $success;
+
+} # try_interaction3
+
+
+
+
 ################################################################################
 1;
+
+
+__END__
+
