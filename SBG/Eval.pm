8db5a89cada9245aabd1da364d8c9c5df3163d82
nobody <nobody@745caa7e-1953-11de-bcda-2bb9902f8029>
2009-01-14 22:47:11 +0000

    Basis for bin/evaluate Utlities for measuring a solution, vs. the native complex structure

---
+++ SBG/Eval.pm
@@ -0,0 +1,82 @@
+#!/usr/bin/env perl
+
+=head1 NAME
+
+SBG::Eval - Evaluation routine to test accuracy of assembly of test complexes
+
+=head1 SYNOPSIS
+
+ use SBG::Eval;
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<SBG::NetworkIO> , L<SBG::ComplexIO>
+
+=cut
+
+################################################################################
+
+package SBG::Eval;
+use SBG::Root -base;
+
+# TODO DES don't need all of these
+our @EXPORT = qw(parse_scopid get_descriptor mk_dom);
+
+use warnings;
+
+use SBG::Domain;
+
+# TODO config.ini
+# Needs to be in MySQL
+my $scopdb = "/home/davis/proj/complexassembly/data/scop_1.73_sorted.dom";
+
+
+################################################################################
+
+# Returns PDBid,chainid,scop_classification
+sub parse_scopid {
+    my $scopid = shift;
+    unless ($scopid =~ /^(\d.{3})(.*?)\.(.*?)$/) {
+        print STDERR "Couldn't parse SCOP ID: $scopid\n";
+        return;
+    }
+    return ($1,$2,$3);
+}
+
+
+
+# Returns filepath,scopid,stamp_descriptor
+sub get_descriptor {
+    my $scopid = shift;
+    # Static opened file handle
+    our $fh;
+    $fh or open $fh, $scopdb;
+    seek $fh, 0, 0;
+    while (<$fh>) {
+        next unless /^(\S+) ($scopid) { (.*?) }$/;
+        return ($1, $2, $3);
+    }
+    return;
+}
+
+
+sub mk_dom {
+    my ($str) = @_;
+    my ($pdbid, $chainid, $scopid) = parse_scopid($str);
+    my ($file, undef, $descriptor) = get_descriptor($str);
+    my $dom = new SBG::Domain(
+        -pdbid=>$pdbid, -chainid=>$chainid, -scopid=>$scopid,
+        -file=>$file, -descriptor=>$descriptor);
+}
+
+
+
+################################################################################
+1;
+
+__END__
+
+
