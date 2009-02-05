993852c742bbec93e553f7d16ba93f0f90f2e940
nobody <nobody@745caa7e-1953-11de-bcda-2bb9902f8029>
2009-02-05 14:59:59 +0000

    Now supports autobox and Moose::Autobox (as ARRAY) Also can still be used functionally

--- SBG/List.pm
+++ SBG/List.pm
@@ -1,13 +1,45 @@
 #!/usr/bin/env perl
 
+=head1 NAME
+
+SBG::List - Utilities on Arrays
+
+=head1 SYNOPSIS
+
+ use SBG::List;
+
+or
+
+use autobox ARRAY => SBG::List;
+
+
+=head1 DESCRIPTION
+
+...
+
+=head1 SEE ALSO
+
+L<List::Utils> , L<List::MoreUtils>
+
+=cut
+
 package SBG::List;
+use Carp;
+use List::Util;
+use List::MoreUtils;
+use Storable;
 use base qw(Exporter);
 
 our @EXPORT = qw(
-nsort
 );
-
 our @EXPORT_OK = qw(
+sum
+min
+max
+uniq
+flatten
+swap
+nsort
 argmax
 argmin
 mean
@@ -16,28 +48,94 @@ variance
 stddev
 sequence
 lcp
-nsort
 intersection
 union
 pairs
 retrieve_files
-rearrange
 reorder
 thresh
-flatten
 );
 
-use List::Util qw(sum);
-use List::MoreUtils qw(uniq);
-use Storable;
-
-# TODO find stdev mean
 
 ################################################################################
 
+# Recursively flattens an array (nested array of arrays) into one long array
+sub flatten { 
+    my @a;
+    foreach (@_) {
+        push @a, (ref($_) eq 'ARRAY') ? flatten(@$_) : $_;
+    }
+    return wantarray ? @a : \@a;
+}
+
+sub sum {
+    return List::Util::sum(flatten @_);
+}
+
+sub min {
+    return List::Util::min flatten @_;
+}
+
+sub max {
+    return List::Util::max flatten @_;
+}
+
+sub uniq { 
+    my @a = List::MoreUtils::uniq flatten @_;
+    return wantarray ? @a : \@a;
+}    
+
+# Numeric sort
+sub nsort {
+    @_ = flatten @_;
+    my @a = sort { $a <=> $b } @_;
+    return wantarray ? @a : \@a;
+}
+
+sub swap (\$\$) {
+    my ($x,$y) = @_;
+    my $c = $$x;
+    $$x = $$y;
+    $$y = $c;
+}
+
+# Permutes a copy, not in-place
+sub permute {
+    @_ = flatten @_;
+    for (0..$#_) {
+        swap($_[$_], $_[rand(@_)]);
+    }
+    return wantarray ? @_ : \@_;    
+}
+
+
+sub argmax(&@) {
+  return() unless @_ > 1;
+  my $block = shift;
+  my $index = shift;
+  my $max = $block->($index);
+  for (@_) {
+    my $val = $block->($_);
+    ($max, $index) = ($val, $_) if $val > $max;
+  }
+  return wantarray ? ($index, $max) : $index;
+}
+
+sub argmin(&@) {
+  return() unless @_ > 1;
+  my $block = shift;
+  my $index = shift;
+  my $min = $block->($index);
+  for (@_) {
+    my $val = $block->($_);
+    ($min, $index) = ($val, $_) if $val < $min;
+  }
+  return wantarray ? ($index, $min) : $index;
+}
 
 # Average of a list
 sub mean {
+    @_ = flatten @_;
     return unless @_;
     return sum(@_) / @_;
 }
@@ -45,13 +143,12 @@ sub avg { return mean @_ }
 
 # Variance of a list 
 sub variance {
-    my @list = flatten(@_);
-    return 0 unless @list > 1;
-    my $sum = 0;
-    my $avg = avg(\@list);
-    my $sumsqdiff = sum map { ($_ - $avg)**2 } @list;
-    # One degree of freedom
-    return $sumsqdiff / (@list - 1);
+    @_ = flatten @_;
+    return 0 unless @_ > 1;
+    my $avg = avg @_;
+    my $sumsqdiff = sum map { ($_ - $avg)**2 } @_;
+    # One degree of freedom, subtract 1
+    return $sumsqdiff / (@_ - 1);
 }
 
 # Stddev of a list
@@ -61,105 +158,13 @@ sub stddev {
 
 # Creates a sequence of numbers (similar to in R)
 sub sequence {
-    my ($start, $inc, $end) = @_;
+    my ($start, $end, $inc) = @_;
+    return $start .. $end unless $inc && $inc > 1;
     my @a;
     for (my $i = $start; $i <= $end; $i+=$inc) {
         push @a, $i;
     }
-    return @a;
-}
-
-
-# Get all the objects from all the Stor'ed arrays in all the given files
-# use Storable
-sub retrieve_files {
-    my @files = @_;
-    my @allobjs;
-    foreach my $file (@files) {
-        # Might be an array, expand/flatten it.
-        push @allobjs, flatten(retrieve $file);
-    }
-    return @allobjs;
-}
-
-
-# One way to support named function parameters. E.g.:
-# func(-param1=>2, -param3=>"house");
-sub rearrange  {
-    # The array ref. specifiying the desired order of the parameters
-    my $order = shift;
-    # Make sure the first parameter, at least, starts with a -
-    return @_ unless (substr($_[0]||'',0,1) eq '-');
-    # Make sure we have an even number of params
-    push @_,undef unless $#_ %2;
-    my %param;
-    while( @_ ) {
-        (my $key = shift) =~ tr/a-z\055/A-Z/d; #deletes all dashes!
-        $param{$key} = shift;
-    }
-    map { $_ = uc($_) } @$order; # for bug #1343, but is there perf hit here?
-    # Return the values of the hash, based on the keys in @$order
-    # I.e. this return the values sorted by the order of the keys
-    return @param{@$order};
-} # rearrange
-
-
-################################################################################
-=head2 reorder
-
- Title   : reorder
- Usage   :
- Function:
- Example :
- Returns : 
- Args    :
-
-The Perl sort() is fine for sorting things alphabeticall/numerically.
-  This is for sorting objects in a pre-defined order, based on some attribute
-Sorts objects, given a pre-defined ordering.
-Takes:
-$objects - an arrayref of objects, in any order
-$accessor - the name of an accessor function to call on each object, like:
-    $_->$accessor()
-Otherwise, standard Perl stringification is used on the objects, i.e. "$obj"
-$ordering - an arrayref of keys (as strings) in the desired order
-  If no ordering given, sorts lexically
-E.g.: 
-NB: duplicate $objects (having the same key) are removed
-
-=cut
-sub reorder {
-    my ($objects, $ordering, $accessor) = @_;
-    # First put the objects into a dictionary, indexed by $accessor
-    my %dict;
-    if ($accessor) {
-        %dict = map { $_->$accessor() => $_ } @$objects;
-    } else {
-        %dict = map { $_ => $_ } @$objects;
-    }
-    # Sort lexically by default
-    $ordering ||= [ sort keys %dict ];
-#     $logger->trace("order by: @$ordering");
-#     $logger->trace("with accessor: $accessor") if $accessor;
-    # Sorted array (of values) based on given ordering (of keys)
-    my @sorted = map { $dict{$_} } @$ordering;
-#     $logger->debug("reorder'ed: @sorted");
-    return \@sorted;
-} # reorder
-
-
-# Converts analogue values to binary, given a threshold
-# Something like this is probably already provided by the PDL
-sub thresh {
-    my ($ref, $thresh) = @_;
-    for (my $i = 0; $i < @$ref; $i++) {
-        $ref->[$i] = $ref->[$i] > $thresh;
-    }
-}
-
-
-sub nsort {
-    return sort { $a <=> $b } @_;
+    return wantarray ? @a : \@a;
 }
 
 
@@ -179,13 +184,15 @@ sub intersection {
     # Which elements exist in each input array
     my @common = grep { $counts{$_} == $n } keys %counts;
     # Get the corresponding values
-    my @objs = map { $things{$_} } @common;
-    return @objs;
+    my @a = map { $things{$_} } @common;
+    return wantarray ? @a : \@a;
 }
 
 
+# Each element in the list(s) will be represented just once, unsorted
 sub union {
-    return uniq(flatten(@_));
+    my @a = uniq(flatten(@_));
+    return wantarray ? @a : \@a;
 }
 
 
@@ -199,18 +206,9 @@ sub pairs {
 }    
 
 
-# Recursively flattens an array (nested array of arrays) into one long array
-sub flatten { 
-    my @a;
-    foreach (@_) {
-        push @a, (ref($_) eq 'ARRAY') ? flatten(@$_) : $_;
-    }
-    return @a;
-}
-
-
 # longest_common_prefix 
 sub lcp {
+    @_ = flatten @_;
     # Start with the shortest prefix
     my @strings = sort { length $a <=> length $b } @_;
     my $prefix = shift @strings;
@@ -223,30 +221,53 @@ sub lcp {
 }
 
 
-sub argmax(&@) {
-  return() unless @_ > 1;
-  my $block = shift;
-  my $index = shift;
-  my $max = $block->($index);
-  for (@_) {
-    my $val = $block->($_);
-    ($max, $index) = ($val, $_) if $val > $max;
-  }
-  return wantarray ? ($index, $max) : $index;
+# Get all the objects from all the Stor'ed arrays in all the given files
+# use Storable
+sub retrieve_files {
+    @_ = flatten @_;
+    my @a = map { flatten Storable::retrieve($_) } @_;
+    return wantarray ? @a : \@a;    
 }
 
 
-sub argmin(&@) {
-  return() unless @_ > 1;
-  my $block = shift;
-  my $index = shift;
-  my $min = $block->($index);
-  for (@_) {
-    my $val = $block->($_);
-    ($min, $index) = ($val, $_) if $val < $min;
-  }
-  return wantarray ? ($index, $min) : $index;
-}
+################################################################################
+=head2 reorder
+
+ Title   : reorder
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+The Perl sort() is fine for sorting things alphabeticall/numerically.
+
+This is for sorting objects in a pre-defined order, based optionally on some
+attribute, which you define by a subroutine that you can pass in.
+
+Takes:
+$objects - an ArrayRef of objects, in any order
+$orderine - an ArrayRef defining the desired order
+$code - a subroutine to apply to the objects, to get the attributes to be sorted
+
+Otherwise, standard Perl stringification is used on the objects, i.e. "$obj"
+$ordering - an arrayref of keys (as strings) in the desired order
+  If no ordering given, sorts lexically
+E.g.: 
+NB: duplicate $objects (having the same key) are removed
+
+=cut
+sub reorder ($;$&) {
+    my ($objects, $ordering, $code) = @_;
+    # First put the objects into a dictionary, after applying code
+    $code ||= sub { "$_" };
+    my %dict = map { $code->($_) => $_ } @$objects;
+    # Sort lexically by default
+    $ordering ||= [ sort keys %dict ];
+    # Sorted array (of values) based on given ordering (of keys)
+    my @sorted = map { $dict{$_} } @$ordering;
+    return wantarray ? @sorted : \@sorted;
+} # reorder
 
 
 ################################################################################
--- SBG/t/List.t
+++ SBG/t/List.t
@@ -1,10 +1,16 @@
 #!/usr/bin/env perl
 
 use Test::More 'no_plan';
+use feature 'say';
+$,=' ';
 
-use strict;
+# Autoboxing native types
+use Moose::Autobox;
+# Also: Look for methods of ARRAY objects in SBG::List
+use autobox ARRAY => SBG::List;
+# Add this in to test
+use SBG::List qw(union intersection nsort flatten lcp pairs mean swap);
 
-use SBG::List qw(union intersection nsort flatten lcp pairs);
 
 my @a = 1..10;
 my @b = 5..15;
@@ -28,6 +34,51 @@ is_deeply([pairs(1..3)], [[1,2],[1,3],[2,3]], "pairs(1..3)");
 is_deeply([pairs(1,2)], [[1,2]], "pairs(1,2)");
 is_deeply([pairs(1)], [], "pairs(1)");
 
+# Text autoboxing
 
-# TODO test reorder()
+my $arrayref = [ 1..10 ];
+$arrayref->push(4);
+is_deeply($arrayref, [1..10,4]);
 
+@vals = 1..4;
+my $rvals = \@vals;
+is(mean(@vals), mean($rvals));
+is(mean(@vals), @vals->mean);
+is(mean($rvals), $rvals->mean);
+is(@vals->mean, $rvals->mean);
+
+# nsort
+$x = [4..10, 8..15,2..6];
+is($x->sum, 161);
+is($x->min, 2);
+is($x->max, 15);
+is_deeply(scalar($x->uniq), [4..15,2..3]);
+is_deeply(scalar($x->union), [4..15,2..3]);
+is_deeply(scalar($x->union->nsort), [2..15]);
+
+# swap scalars
+my ($a, $b) = 1..2;
+swap($a,$b);
+is($a,2);
+is($b,1);
+
+$x = $x->uniq;
+$xs = $x->sort;
+$perm = $x->permute;
+$perms = $perm->sort;
+is_deeply($xs, $perms);
+
+
+# Some objects
+$objs = [ { name=>'joe'}, {name=>'alice'}, {name=>'frank'} ];
+# An accessor
+my $name = sub { (shift)->{name} };
+# Original order of mapped variable
+my $orign = $objs->map($name);
+# Order we will request (a boring example: lexicographic)
+my $sortedn = $orign->sort;
+# Reorder $objs, after applying $name->(), according to $sortedn
+$r = $objs->reorder($sortedn, $name);
+# Check that the resulting objects are in the right order, by checking the names
+$rnames = $r->map($name);
+is_deeply($rnames, $sortedn);
