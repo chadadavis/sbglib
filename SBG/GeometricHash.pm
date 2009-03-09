9a9bcad2d3c98911972c65be178359bc62aaa2f6
nobody <nobody@745caa7e-1953-11de-bcda-2bb9902f8029>
2009-03-09 07:26:09 +0000

    Binning function for matrices (vector of 3D points)

--- SBG/GeometricHash.pm
+++ SBG/GeometricHash.pm
@@ -22,19 +22,41 @@ SBG::GeometricHash -
 
 1. Wolfson, H. & Rigoutsos, I. Geometric hashing: an overview. Computational Science & Engineering, IEEE 4, 10-21(1997).
 
+The interface is based on L<Moose::Autobox::Hash>
+
+       at
+       put
+       exists
+       keys
+       values
+       kv
+       slice
+       meta
+       print
+       say
+
 =cut
 
 ################################################################################
 
 package SBG::GeometricHash;
-use Moose;
 
-with 'SBG::Storable';
-with 'SBG::Dumpable';
+use PDL::Lite;
+use PDL::Core;
+use PDL::Matrix;
+use PDL::Ufunc;
 
 
-################################################################################
-# Accessors
+
+
+
+sub new {
+    my ($class) = @_;
+    my $self = bless {}, $class;
+    $self->{_gh} = {};
+    return $self;
+}
+
 
 
 ################################################################################
@@ -46,12 +68,13 @@ with 'SBG::Dumpable';
  Function: 
  Example : 
  Returns : 
- Args    : 
+ Args    : $model a L<PDL::Matrix> of dimension nx3
 
+Returns a hash of votes for specific models.
 
 =cut
 sub at {
-    my ($self,$points) = @_;
+    my ($self,$model) = @_;
 
 } # at
 
@@ -67,7 +90,14 @@ sub at {
 
 =cut
 sub put {
-    my ($self,$points,$model) = @_;
+    my ($self,$points,$modelid) = @_;
+
+    foreach () {
+        # Note not to quantize the addt'l rows/cols in a homogenous matrix
+        $qpoint = _quantize($point);
+        $self->{_gh}{$qpoint} ||= [];
+        push @{ $self->{_gh}{$qpoint} }, $modelid;
+    }
 
 
 } # put
@@ -77,8 +107,76 @@ sub put {
 ################################################################################
 # Private
 
+################################################################################
+=head2 _quantize
+
+ Function: 
+ Example : 
+ Returns : 
+ Args    : 
+
+For example, with binsize=5, converts:
+
+ [ 6.1724927 -1.3568784]
+ [-5.2222763  18.794775]
+
+to:
+
+ [ 5  0]
+ [-5 15]
+
+NB The bin that straddles 0 will be twice the size it should be.  This is
+because floor() is not applicable to PDL data. Instead we use PDL::long() which
+just converts to an integer.
+
+=cut
+# NB This is done inplace. I.e. $matrix is modified
+sub _quantize {
+    my ($matrix, $binsize) = @_;
+    $binsize ||= 1;
+
+    # inplace division
+    $matrix /= $binsize;
+    # convert to integer
+    $matrix = long $matrix;
+    # inplace multiplication
+    $matrix *= $binsize;
+    return $matrix;
+
+}
+
 
 ################################################################################
-__PACKAGE__->meta->make_immutable;
+=head2 _transform
+
+ Function: 
+ Example : 
+ Returns : 
+ Args    : 
+
+Determine the basis transformation, which is the transformation that puts the
+first point at the origin and the second point at X=1
+
+
+=cut
+sub _transform {
+    my ($self,$points) = @_;
+
+    my $t = mpdl [ [1,0,0,0], [0,1,0,0], [0,0,1,0], [0,0,0,1] ];
+
+    # First translate the first point to the origin
+
+    # Then rotate about the origin to get the second point into the X axis
+
+    # Two separate rotations required to get into X axis:
+
+    # Scale down to unit length
+
+    return $t;
+
+} # _transform
+
+
+################################################################################
 1;
 
