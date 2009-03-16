98476b87c9bc47bcc8407ad6f73be2803455af28
nobody <nobody@745caa7e-1953-11de-bcda-2bb9902f8029>
2009-03-16 18:29:24 +0000

    Now returning (7) ATOM lines in 'description' field (contains "\n") for the cross-hairs.
    BUG: (outstanding) _run doesn't parse insertion codes, when present

--- SBG/Run/cofm.pm
+++ SBG/Run/cofm.pm
@@ -8,8 +8,8 @@ SBG::Run::cofm - Wrapper for running B<cofm> (centre-of-mass), with DB caching.
 =head1 SYNOPSIS
 
  use SBG::Run::cofm;
- my ($x,$y,z,$rg,$max) = 
-    SBG::Run::cofm::run('2nn6','A 13 _ to A 331 _');
+ $hashref = SBG::Run::cofm::run('2nn6','A 13 _ to A 331 _');
+ $hashref = SBG::Run::cofm::run('2nn6','CHAIN A CHAIN B');
 
 
 =head1 DESCRIPTION
@@ -32,13 +32,16 @@ use base qw/Exporter/;
 our @EXPORT_OK = qw/cofm/;
 
 use Carp;
+use IO::String;
+use PDL::Lite;
+use PDL::Matrix;
+use Text::ParseWords;
+
 use SBG::Types qw/$re_chain_id/;
 use SBG::DB::cofm;
 use SBG::Config qw/val/;
 use SBG::Domain;
 use SBG::DomainIO;
-
-use Text::ParseWords;
 
 
 ################################################################################
@@ -49,8 +52,8 @@ use Text::ParseWords;
  Returns : HashRef with keys: Cx, Cy,Cz, Rg, Rmax, file, descriptor
  Args    : 
 
-If the Domain is an entire chain, the database cache is queried first. Otherwise
-cofm is run locally, if available. 
+If the Domain is one entire chain, the database cache is queried
+first. Otherwise cofm is run locally, if available.
 
 B<cofm> must be in your PATH, or defined in a B<config.ini> file
 
@@ -60,32 +63,33 @@ The DB cache stores uppercase PDB IDs. The B<cofm> program will accept any case.
 sub cofm {
     my ($pdbid, $descriptor) = @_;
     return unless $pdbid && $descriptor;
-    my ($chainid) = $descriptor =~ /^\s*CHAIN\s+($re_chain_id)\s*$/;
+    my $dom = new SBG::Domain(pdbid=>$pdbid, descriptor=>$descriptor);
 
     my $res;
     # If descriptor contains just one full chain, try the cache first;
+    my $chainid = $dom->wholechain();
     $res = SBG::DB::cofm::query($pdbid, $chainid) if $chainid;
 
     # Couldn't get from DB, try running computation locally, on descriptor
-    $res = run($pdbid, $descriptor) unless defined($res);
+    $res = _run($pdbid, $descriptor) unless defined($res);
 
     unless ($res) {
         carp "Cannot get centre-of-mass for ${pdbid} \{ ${descriptor} \}";
         return;
     }
 
-    # keys: (Cx, Cy,Cz, Rg, Rmax, file, descriptor)
+    # keys: (Cx, Cy, Cz, Rg, Rmax, description, file, descriptor)
     return $res;
 
 } # cofm
 
 
 ################################################################################
-=head2 run
+=head2 _run
 
  Function: Computes centre-of-mass and radius of gyration of STAMP domain
  Example : 
- Returns : HashRef with keys: Cx, Cy,Cz, Rg, Rmax, file, descriptor
+ Returns : HashRef with keys: Cx, Cy,Cz, Rg, Rmax, description, file, descriptor
  Args    : L<SBG::Domain>
 
 Runs external B<cofm> appliation. Must be in your environment's B<$PATH>
@@ -112,28 +116,24 @@ And then, a few lines later:
 
  REMARK Domain 1 Id 1li4a N = 860 Rg = 34.441 Rmax = 58.412 Ro = 44.760 0.000 85.323
 
+All of the ATOM lines are saved, as new-line separated text in the 'description'
+
+BUG: Insertion code ignored by cofm?
+No, just not always there ...:
+        from B  100   to B  131 A  33 CAs =>   33 CAs in total
 
 =cut
-sub run {
+sub _run {
     my ($pdbid, $descriptor) = @_;
     return unless $pdbid && $descriptor;
-    
-    my $dom = new SBG::Domain(pdbid=>$pdbid,descriptor=>$descriptor);
-    my $io = new SBG::DomainIO(tempfile=>1);
-    my $path = $io->file;
-    $io->write($dom);
-    $io->close;
-    unless (-s $path) {
-        carp "Failed to write Domain to tempfile: $path";
-        return;
-    }
+    # Get dom into a stamp-formatted file
+    my $path = _spitdom($pdbid, $descriptor) or return;
 
     # NB the -v option is necessary if you want the filename of the PDB file
     my $cofm = val(qw/cofm executable/) || 'cofm';
     my $cmd = "$cofm -f $path -v |";
-
-    my $cofmfh;
-    unless (open $cofmfh, $cmd) {
+    open my $cofmfh, $cmd;
+    unless ($cofmfh) {
         carp "Failed:\n\t$cmd\n\t$!";
         return;
     }
@@ -146,7 +146,9 @@ sub run {
             # Maybe multiple chains here
             my @tchs = map { "CHAIN $_" } @chs;
             $res{descriptor} = join(' ', @tchs);
+            
         } elsif (my @descrs = /from\s+(\S+)\s+(\S+)\s+to\s+\1\s+(\S+)/ig) {
+            # TODO BUG What about insertion code here?
             # Maybe multiple segments here. Capture all and re-format
             my @groupdescrs;
             while (@descrs) {
@@ -165,7 +167,10 @@ sub run {
             $res{Rg} = $a[10];
             $res{Rmax} = $a[13];
             ($res{Cx}, $res{Cy}, $res{Cz}) = ($a[16], $a[17], $a[18]);
+        } elsif (/^ATOM/) {
+            $res{description} .= $_;
         }
+        
     }
 
     unless (%res) {
@@ -176,10 +181,83 @@ sub run {
     carp "Descriptors unequal:$descriptor:$res{descriptor}:" unless
         $descriptor eq $res{descriptor};
 
-    # keys: (Cx, Cy,Cz, Rg, Rmax, file, descriptor)
+    # keys: (Cx, Cy,Cz, Rg, Rmax, description, file, descriptor)
     return \%res;
 
 } # run
+
+
+
+################################################################################
+=head2 _spitdom
+
+ Function: 
+ Example : 
+ Returns : 
+ Args    : 
+
+Dumps domain in STAMP format to file.
+
+=cut
+sub _spitdom {
+    my ($pdbid, $descriptor) = @_;
+    my $dom = new SBG::Domain(pdbid=>$pdbid,descriptor=>$descriptor);
+    my $io = new SBG::DomainIO(tempfile=>1);
+    my $path = $io->file;
+    $io->write($dom);
+    $io->close;
+    unless (-s $path) {
+        carp "Failed to write Domain to tempfile: $path";
+        return;
+    }
+    return $path;
+}
+
+
+################################################################################
+=head2 _atom2pdl
+
+ Function: 
+ Example : 
+ Returns : 
+ Args    : 
+
+Parses ATOM lines and converts to nx3-dimensional L<PDL::Matrix>
+
+Any lines not beginning wth ATOM are skipped.
+
+E.g.:
+
+ ATOM      0  CA  ALA Z   0      80.861  12.451 122.080  1.00 10.00
+ ATOM      1  CA  ALA Z   1      85.861  12.451 122.080  1.00 10.00
+ ATOM      1  CA  ALA Z   1      75.861  12.451 122.080  1.00 10.00
+ ATOM      2  CA  ALA Z   2      80.861  17.451 122.080  1.00 10.00
+ ATOM      2  CA  ALA Z   2      80.861   7.451 122.080  1.00 10.00
+ ATOM      3  CA  ALA Z   3      80.861  12.451 127.080  1.00 10.00
+ ATOM      3  CA  ALA Z   3      80.861  12.451 117.080  1.00 10.00
+
+31 - 38        Real(8.3)     x            Orthogonal coordinates for X in Angstroms.
+39 - 46        Real(8.3)     y            Orthogonal coordinates for Y in Angstroms.
+47 - 54        Real(8.3)     z            Orthogonal coordinates for Z in Angstroms.
+
+TODO DES belongs in a more general module. At least Domain::CofM
+
+=cut
+sub _atom2pdl {
+    my ($atomstr) = @_;
+    my $io = new IO::String($atomstr);
+    my @mat;
+    for (my $i = 0; <$io>; $i++) {
+        next unless /^ATOM/;
+        my $str = $_;
+        # Columns 31,39,47 store the 8-char coords (not necessarily separated)
+        # substr() is 0-based
+        my @xyz = map { substr($str,$_,8) } (30,38,46);
+        # Append array with an arrayref of X,Y,Z fields
+        push @mat, [ @xyz ];
+    }
+    return mpdl @mat;
+}
 
 
 ################################################################################
--- SBG/t/Run::cofm.t
+++ SBG/t/Run::cofm.t
@@ -6,12 +6,13 @@ use SBG::Run::cofm qw/cofm/;
 use SBG::Test qw/float_is/;
 $, = ' ';
 
-
+# Precision
 my $prec = 4;
 
+# Don't use a whole chain here, as those are cached in the DB
 $res = cofm('2nn6', "A 50 _ to A 120 _");
-($tx, $ty, $tz, $trg, $trmax) = (83.495, 17.452, 114.562, 15.246, 22.781);
 
+($tx, $ty, $tz, $trg, $trmax) = (83.495, 17.452, 114.562, 15.246, 22.781);
 float_is($res->{Cx}, $tx, $prec);
 float_is($res->{Cy}, $ty, $prec);
 float_is($res->{Cz}, $tz, $prec);
@@ -20,21 +21,18 @@ float_is($res->{Rmax}, $trmax, $prec);
 is($res->{descriptor}, 'A 50 _ to A 120 _', 'descriptor');
 ok($res->{file}, "File: $file");
 
-say Dumper $res;
-__END__
+my @atomlines = $res->{'description'} =~ /^ATOM/gm;
+is(scalar(@atomlines), 7, "7 Points: CofM,X+/-5,Y+/-5,Z+/-5");
 
-$res = cofm('2nn6', 'CHAIN A');
-($tx, $ty, $tz, $trg, $trmax) = (80.860, 12.450, 122.080, 26.738, 63.826);
+my $m = SBG::Run::cofm::_atom2pdl($res->{'description'});
 
-float_is($res->{Cx}, $tx, $prec);
-float_is($res->{Cy}, $ty, $prec);
-float_is($res->{Cz}, $tz, $prec);
-float_is($res->{Rg}, $trg, $prec);
-float_is($res->{Rmax}, $trmax, $prec);
-is($res->{descriptor}, 'CHAIN A', 'descriptor');
-ok($res->{file}, "File: $file");
+print $m;
 
-# TODO test on multi-chain or multi-segment domains
+TODO: {
+    local $TODO;
+    $TODO = "test on multi-chain or multi-segment domains";
+    ok(0);
+}
 
 
 __END__
