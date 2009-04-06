587bd3b7c07d0551c226e0972f3b2e7d8571dc4e
chad.a.davis <chad.a.davis@745caa7e-1953-11de-bcda-2bb9902f8029>
2009-04-06 16:30:09 +0000

    Bash programmable completion for script/sbg Requires source'ing env.bash

--- env.bash
+++ env.bash
@@ -8,9 +8,10 @@ BASE=`dirname ${BASH_SOURCE[0]}`
 export PERL5LIB="$PERL5LIB:$BASE/lib"
 
 # If STAMPDIR is not already set, default:
-${STAMPDIR:=/g/russell1/apps/stamp.4.3/defs}
-export STAMPDIR
+export STAMPDIR=${STAMPDIR:=/g/russell1/apps/stamp.4.3/defs}
 
 export PATH=$PATH:/g/russell1/lbin
+
+complete -o default -C 'sbg -options' sbg
 
 
