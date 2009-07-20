c25d6bef2ed05c0632714ca13e1fbdace5fc875d
chad.a.davis <chad.a.davis@745caa7e-1953-11de-bcda-2bb9902f8029>
2009-07-20 10:13:33 +0000

    Eval object. To be Storable.

--- sbglib/branches/davis-role-refactor2/lib/SBG/Eval.pm
+++ sbglib/branches/davis-role-refactor2/lib/SBG/Eval.pm
@@ -20,70 +20,19 @@ L<SBG::NetworkIO> , L<SBG::ComplexIO>
 ################################################################################
 
 package SBG::Eval;
-use SBG::Root -base;
+use Moose;
 
-# TODO DES don't need all of these
-our @EXPORT = qw(parse_scopid get_descriptor mk_dom);
-
-use warnings;
-
-use SBG::Domain;
-
-# TODO config.ini
-# Needs to be in MySQL
-my $scopdb = "/g/russell1/data/pdbrep/scop_1.73.dom";
+with 'SBG::Role::Storable';
+with 'SBG::Role::Clonable';
 
 
-################################################################################
-
-# Returns PDBid,chainid,scop_classification
-# TODO DES move to SCOP
-sub parse_scopid {
-    my $scopid = shift;
-    unless ($scopid =~ /^(\d.{3})(.*?)\.(.*?)$/) {
-        print STDERR "Couldn't parse SCOP ID: $scopid\n";
-        return;
-    }
-    return ($1,$2,$3);
-}
-
-
-
-# Returns filepath,scopid,stamp_descriptor
-# TODO DES move to SCOP
-sub get_descriptor {
-    my $scopid = shift;
-    # Static opened file handle
-    our $fh;
-    unless ($fh) {
-        unless (open $fh, $scopdb) {
-            $logger->error("Cannot open: $scopdb ($!)");
-            return;
-        }
-    }
-    seek $fh, 0, 0;
-    while (my $_ = <$fh>) {
-        next unless /^(\S+) ($scopid) { (.*?) }$/;
-        return ($1, $2, $3);
-    }
-    return;
-}
-
-# TODO DES move to SCOP
-sub mk_dom {
-    my ($str) = @_;
-    my ($pdbid, $chainid, $scopid) = parse_scopid($str);
-    my ($file, undef, $descriptor) = get_descriptor($str);
-    my $dom = new SBG::Domain(
-        -pdbid=>$pdbid, -chainid=>$chainid, -scopid=>$scopid,
-        -file=>$file, -descriptor=>$descriptor);
-}
 
 
 
 ################################################################################
+__PACKAGE__->meta->make_immutable;
+no Moose;
 1;
 
-__END__
 
 
