68f1d011e7879c29e2b3a8d7b073241175470764
chad.a.davis@gmail.com <chad.a.davis@gmail.com@745caa7e-1953-11de-bcda-2bb9902f8029>
2009-11-08 17:15:01 +0000

    float_is now also accepts a tolerance of the form: '15%' to indicate a deviation from the the first value $val1

--- sbglib/trunk/lib/SBG/U/Test.pm
+++ sbglib/trunk/lib/SBG/U/Test.pm
@@ -42,14 +42,24 @@ our @EXPORT_OK = qw(float_is pdl_approx pdl_percent);
 sub float_is ($$;$$) {
    my ($val1, $val2, $msg, $tol) = @_;
    return unless defined($val1) && defined($val2);
-   $tol = 1.0 unless defined $tol;
+   $tol = '10%' unless defined $tol;
+
    my $diff = abs($val1-$val2);
-   $msg ||= "float_is: $diff < $tol";
-   if(ok($diff < $tol, $msg)) {
+   $msg ||= "float_is: $diff < $tol (from $val1)";
+
+   my $ok;
+   if ($tol =~ /(\d+)\%$/) {
+       my $perc = $1 / 100.0;
+       $ok = ok($diff < $perc * $val1, $msg);
+   } else {
+       $ok = ok($diff < $tol, $msg);
+   }
+
+   if($ok) {
        return 1;
    } else {
        printf STDERR 
-           "\t|%g - %g| == %g exceeds tolerance: %g\n", 
+           "\t|%g - %g| == %g exceeds tolerance: %s\n", 
            $val1, $val2, $diff, $tol;
    }
 }
