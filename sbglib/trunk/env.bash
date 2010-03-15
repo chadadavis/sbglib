019a1d0f190efe7f46ce61f514200564d6583e55
chad.a.davis <chad.a.davis@745caa7e-1953-11de-bcda-2bb9902f8029>
2010-03-15 17:16:58 +0000

    STAMP path etc are not part of sbglib

--- sbglib/trunk/env.bash
+++ sbglib/trunk/env.bash
@@ -2,19 +2,6 @@
 # Library paths are always appended rathern than prepended.
 # They will not over-ride your own libraries
 
-# Get location of this script (bash ver 2 cannot do this)
-if ((${BASH_VERSINFO[0]} >= 3)); then
-    BASE=`dirname ${BASH_SOURCE[0]}`
-    export PERL5LIB="$PERL5LIB:$BASE/lib"
-fi
-
-# If STAMPDIR is not already set, default:
-if [ -z "$STAMPDIR" ]; then 
-    export STAMP="/g/russell1/apps/stamp.4.4"
-    export STAMPDIR="$STAMP/defs"
-    export PATH=$PATH:$STAMP/bin/`uname -m`
-fi
-
 # Enable bash programable completion
 shopt -s progcomp
 # And don't break words on a colon :
