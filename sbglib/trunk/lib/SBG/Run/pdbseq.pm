49788e80777982ac46f6b7e6410545e060ac44f2
chad.a.davis <chad.a.davis@745caa7e-1953-11de-bcda-2bb9902f8029>
2010-03-16 15:44:17 +0000

    Explicitly flushing IO to external programs

--- sbglib/trunk/lib/SBG/Run/pdbseq.pm
+++ sbglib/trunk/lib/SBG/Run/pdbseq.pm
@@ -44,12 +44,12 @@ sub pdbseq {
     my (@doms) = @_;
     my $domio = SBG::DomainIO::stamp->new(tempfile=>1);
     $domio->write($_) for @doms;
+    $domio->close; # Flush
     my $dompath = $domio->file;
     my (undef, $fapath) = tempfile(TMPDIR=>1);
     my $seqstr = `pdbseq -min 1 -f $dompath`;
     return unless $seqstr && $seqstr =~ /^>/;
     my $instr = IO::String->new($seqstr); 
-#    my $faio = Bio::SeqIO->new(-file=>$fapath, -format=>'Fasta');
     my $faio = Bio::SeqIO->new(-fh=>$instr, -format=>'Fasta');
     my @seqs;
     while (my $seq = $faio->next_seq) {
