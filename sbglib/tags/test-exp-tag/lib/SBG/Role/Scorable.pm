e0c71560c7fd3ba87e5dd8826562110266bf4ded
chad.a.davis <chad.a.davis@745caa7e-1953-11de-bcda-2bb9902f8029>
2010-03-16 12:39:37 +0000

    Testing tagging per experiment

---
+++ sbglib/tags/test-exp-tag/lib/SBG/Role/Scorable.pm
@@ -0,0 +1,90 @@
+#!/usr/bin/env perl
+
+=head1 NAME
+
+SBG::Role::Scorable - 
+
+=head1 SYNOPSIS
+
+with 'SBG::Role::Scorable';
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+
+=cut
+
+################################################################################
+
+package SBG::Role::Scorable;
+use Moose::Role;
+
+# Also a functional interface
+use base qw/Exporter/;
+our @EXPORT_OK = qw/group_scores/;
+
+use Moose::Autobox;
+
+
+################################################################################
+=head2 scores
+
+ Function: 
+ Example : 
+ Returns : 
+ Args    : 
+
+
+=cut
+has 'scores' => (
+    is => 'rw',
+    isa => 'HashRef',
+    lazy_build => 1,
+    );
+sub _build_scores {
+    return {} ;
+}
+
+
+################################################################################
+=head2 group_scores
+
+ Function: 
+ Example : 
+ Returns : 
+ Args    : 
+
+Given an array of Scorable objects, extract the 'scores' and populate a
+Hashref of ArrayRefs
+
+E.g. converts
+
+ [ { scores=>{size=>10,weight=>3} } , { scores=>{size=>7,weight=>2} } ]
+
+to
+
+ { size=>[10,7], weight=>[3,2] }
+ 
+
+=cut
+sub group_scores {
+    my ($array) = @_;
+    my $hash = {};
+    foreach my $eachhash ($array->flatten) {
+        foreach my $key ($eachhash->keys->flatten) {
+            $hash->{$key} ||= [];
+            $hash->{$key}->push($eachhash->{$key});
+        }
+    }
+    return $hash;
+} # group_scores
+
+
+
+################################################################################
+no Moose::Role;
+1;
+
+
