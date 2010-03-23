ea503ba615a57a4d5d31762f150ef27bd3f66fee
chad.a.davis <chad.a.davis@745caa7e-1953-11de-bcda-2bb9902f8029>
2010-03-23 20:36:42 +0000

    Tagged 2010-03-23-tac3-bench-knowns

---
+++ sbglib/tags/2010-03-23-tac3-bench-knowns/trunk/lib/SBG/Role/Clonable.pm
@@ -0,0 +1,53 @@
+#!/usr/bin/env perl
+
+=head1 NAME
+
+SBG::Role::Clonable - Role implementing B<Storable::dclone>
+
+=head1 SYNOPSIS
+
+ with 'SBG::Role::Clonable';
+ with 'SBG::Role::Clonable' => { depth => 2 }; # default
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<Storable>
+
+=cut
+
+################################################################################
+
+package SBG::Role::Clonable;
+use Moose::Role;
+
+use Storable qw/dclone/;
+
+
+################################################################################
+=head2 clone
+
+ Function: 
+ Example : 
+ Returns : 
+ Args    : 
+
+Calls 
+
+ Storable::dclone($self)
+
+which allows one to define hooks for Storable to override the default cloning.
+
+=cut
+sub clone {
+    my ($self) = @_;
+    return dclone($self);
+}
+
+################################################################################
+no Moose::Role;
+1;
+
+
