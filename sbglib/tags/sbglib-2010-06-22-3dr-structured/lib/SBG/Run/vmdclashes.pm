730e268d732e154130f22291d3e41b9aa815f097
chad.a.davis <chad.a.davis@745caa7e-1953-11de-bcda-2bb9902f8029>
2010-07-05 11:05:11 +0000

    Clash detection based on external VMD system call. Uses text-mode-only VMD on non-OpenGL cluster nodes.

---
+++ sbglib/tags/sbglib-2010-06-22-3dr-structured/lib/SBG/Run/vmdclashes.pm
@@ -0,0 +1,71 @@
+#!/usr/bin/env perl
+
+=head1 NAME
+
+SBG::Run::pdbc - Wrapper for running B<pdbc> (to get entry/chain descriptions
+
+
+=head1 SYNOPSIS
+
+ use SBG::Run::pdbc qw/pdbc/;
+
+ my $dom = new SBG::DomainI(pdbid=>'2nn6', descriptor=>'A 13 _ to A 331 _');
+ my %fields = pdbc($dom);
+ print "Entry description:", $fields{'header'};
+ print "Chain A description:", $fields{'A'};
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<SBG::DomainI>
+
+=cut
+
+
+
+package SBG::Run::vmdclashes;
+use base qw/Exporter/;
+our @EXPORT = qw/vmdclashes/;
+
+use Moose::Autobox;
+use Log::Any qw/$log/;
+
+use File::Basename qw/dirname/;
+
+use SBG::ComplexIO::pdb;
+
+
+=head2 vmdclashes
+
+ Function: 
+ Example : 
+ Returns : Hash
+ Args    : L<SBG::Complex>
+
+
+=cut
+sub vmdclashes {
+	my ($complex) = @_;
+	my $io = SBG::ComplexIO::pdb->new(tempfile=>1);
+	$io->write($complex);
+	$io->close;
+	my $file = $io->file;
+	
+    my $script = dirname(__FILE__) . '/../../../script/vmdclashes.tcl';
+	my $cmd = "vmd -dispdev none -e $script -args $file";
+    $log->debug($cmd);	
+	my $res = {};
+	open my $vmdfh, "$cmd |";
+	while (<$vmdfh>) {
+		next unless /=/;
+		chomp;
+		my ($key, $value) = split /=/;
+		$res->{$key} = $value;
+	}
+	close $vmdfh;
+	return $res;
+	
+} # vmdclashes
+
---
+++ sbglib/tags/sbglib-2010-06-22-3dr-structured/t/SBG/Run/vmdclashes.t
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Test::More;
+use Data::Dumper;
+use Data::Dump qw/dump/;
+
+use FindBin qw/$Bin/;
+use lib "$Bin/../../../lib/";
+use SBG::U::Test qw/float_is pdl_approx/;
+use SBG::U::Log;
+
+my $DEBUG;
+#$DEBUG = 1;
+SBG::U::Log::init( undef, loglevel => 'DEBUG' ) if $DEBUG;
+
+use SBG::U::Object qw/load_object/;
+use SBG::Run::vmdclashes;
+
+# Precision (or error tolerance)
+my $prec = '2%';
+
+my $complex = load_object("$Bin/../data/086-00002.model");
+my $res = vmdclashes($complex);
+
+float_is($res->{'pcclashes'}, 0.33969166, "vmdclashes", $prec);
+
+done_testing();
