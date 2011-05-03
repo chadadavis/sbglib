eff64d6fdcde95fc49ce471bf32a0938d4b62a1b
chad.a.davis <chad.a.davis@745caa7e-1953-11de-bcda-2bb9902f8029>
2011-05-03 13:53:19 +0000

    Demo of STAMP for all-against-all transformations

---
+++ sbglib/trunk/script/stamp-demo.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/env perl
+use strict;
+use SBG::STAMP qw/superposition/;
+use SBG::Domain;
+
+# Default options: 
+#         "-s",               # scan mode: only query compared to each DB sequence
+#         "-secscreen F",     # Do not perform initial secondary structure screen
+#         "-opd",             # one-per-domain: just one hit per query domain
+#         "-n $nfit",         # $SBG::STAMP::nfit = 2
+#         "-slide $slide",    # $SBG::STAMP::slide = 5 
+#         "-minfit $minfit",  # $SBG::STAMP::minfit = 30
+#         "-scancut $scancut",#$SBG::STAMP::scancut = 2.0
+# To change them:
+# $SBG::STAMP::prameters = "-s -n 3 -scancut 3";
+
+# Example:
+my $data = [
+    { pdbid=>'1tim', descriptor=>'CHAIN A' },
+    { pdbid=>'1tim', descriptor=>'CHAIN B' },
+    { pdbid=>'1dkw', descriptor=>'CHAIN A' },
+    { pdbid=>'1dkw', descriptor=>'CHAIN B' },
+    # ...
+    ];
+    
+    
+# All pairs
+for (my $i = 0; $i < @$data; $i++) {
+    my $domi = SBG::Domain->new(
+        pdbid=>$data->[$i]->{pdbid}, descriptor=>$data->[$i]->{descriptor});
+    
+    for (my $j = $i + 1; $j < @$data; $j++) {
+        my $domj = SBG::Domain->new(
+            pdbid=>$data->[$j]->{pdbid}, descriptor=>$data->[$j]->{descriptor});
+        
+            # This is always the transformation from i onto j (j is fixed)
+            my $super = superposition($domi, $domj);
+            
+            # If you also want to apply the transformation permanently
+            # $super->transformation->apply($domi);
+            
+            # See SBG::Superposition for details
+            # E.g. 'RMS', 'Sc', ...
+            print join(' ', %{$super->scores}), "\n";
+            
+            # Prints the matrix, which is in $super->transformation
+            # It's an instance of SBG::Transformation
+            # The raw matrix (a PDL) would be: $super->transformation->matrix
+#            print $super, "\n";          
+            
+            # if you want it in block formatted text with newlines
+            use SBG::TransformIO::stamp;
+            my $string;
+            my $io = SBG::TransformIO::stamp->new(string=>\$string);
+            $io->write($super->transformation);
+            print $string; 
+    }
+}    
