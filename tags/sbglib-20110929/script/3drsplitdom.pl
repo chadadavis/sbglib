6087b47786ba4296cd3fc9c8738828446c24f1d0
chad.a.davis <chad.a.davis@745caa7e-1953-11de-bcda-2bb9902f8029>
2011-09-29 09:22:16 +0000

    Tagging trunk before it was accidentally deleted

---
+++ tags/sbglib-20110929/script/3drsplitdom.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/env perl
+
+=head1 NAME
+
+B<sbgsplitdom> - 
+
+=head1 SYNOPSIS
+
+sbgsplitdom < fasta1.fa  > fastasplit.fa
+
+=head1 DESCRIPTION
+
+
+=head1 OPTIONS
+
+=head2 -h|elp Print this help page
+
+=head2 -l|og Set logging level
+
+
+=head1 SEE ALSO
+
+L<SBG::SplitI>,
+
+=cut 
+
+use strict;
+use warnings;
+
+use Moose::Autobox;
+use Bio::SeqIO;
+
+# Local libraries
+use FindBin qw/$Bin/;
+use lib "$Bin/../lib/";
+
+use SBG::U::Run qw/getoptions/;
+use SBG::Split::3DR;
+
+# Read from STDIN or from a (single) file
+my $file = shift;
+open *STDIN, '<', $file if $file;
+
+my %ops = getoptions qw/mingap|g=i/;
+$ops{mingap} = 30 unless defined $ops{mingap};
+my $in = Bio::SeqIO->new(-fh => \*STDIN);
+my $out = Bio::SeqIO->new(-fh => \*STDOUT, -format => 'fasta');
+my $splitter = SBG::Split::3DR->new(mingap => $ops{mingap});
+
+while (my $seq = $in->next_seq) {
+    my $subseqs = $splitter->split($seq);
+    $out->write_seq($_) for @$subseqs;
+}
+
+exit;
+
